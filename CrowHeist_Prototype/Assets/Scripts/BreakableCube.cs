a580fe58c86add44858dbe18caf0b9a8c26870f1
SpaceDividedbyTime <hayjus0@gmail.com>
2025-03-04 16:09:57 -0500

    Broken glass maintains velocity
     (makes it feel better to break)

--- CrowHeist_Prototype/Assets/Scripts/BreakableCube.cs
+++ CrowHeist_Prototype/Assets/Scripts/BreakableCube.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BreakableCube : MonoBehaviour
 {
     public GameObject brokenPrefab; // Assign the broken cube prefab in the Inspector
     public float breakForce = 5f; // Velocity needed to break
+    public Vector3[] velocities = new Vector3[3] { Vector3.zero, Vector3.zero, Vector3.zero };
+
+    private void FixedUpdate()
+    {
+        Vector3[] hold = new Vector3[3];
+
+        hold[0] = GetComponent<Rigidbody>().velocity;
+        hold[1] = velocities[0];
+        hold[2] = velocities[1];
+
+        velocities = hold;
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -20,9 +33,19 @@ public class BreakableCube : MonoBehaviour
         // Spawn the broken version
         GameObject broken = Instantiate(brokenPrefab, transform.position, transform.rotation);
 
+        Vector3 max = Vector3.zero;
+
+        foreach(Vector3 x in velocities){
+            if(x.magnitude > max.magnitude)
+            {
+                max = x;
+            }
+        }
+
         // Get all Rigidbody components and add force for explosion effect
         foreach (Rigidbody rb in broken.GetComponentsInChildren<Rigidbody>())
         {
+            rb.velocity = max;
             rb.AddExplosionForce(5f, transform.position, 2f);
         }
 
