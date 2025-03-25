3df7fd12821039b10d661ea21ce134c7db34e566
horvj <horvathjack1@gmail.com>
2025-03-25 18:39:32 -0400

    updated functionality for jack in the box
    jack in the box now pops open when applying force to all gameobjects. also fixed bug making it so that the jackinthebox does not continously apply force to gameobjects with rigidbodys attached.

--- CrowHeist_Prototype/Assets/Scripts/JackInTheBox.cs
+++ CrowHeist_Prototype/Assets/Scripts/JackInTheBox.cs
@@ -6,25 +6,31 @@ using UnityEngine;
 public class JackInTheBox : MonoBehaviour
 {
     public bool bounce = false;
-    private Vector3 launchForce = new Vector3(0, 10f, 0);
+    private Vector3 launchForce = new Vector3(0, 30f, 0);
     public GameObject player;
-    private Controller2Point5D playerController; 
+    private Controller2Point5D playerController;
+    private Transform jack;
 
     void Start()
     {
-        if(player != null)
+        jack = transform.Find("SpringFunction");
+
+        if (player != null)
         {
             playerController = player.GetComponent<Controller2Point5D>();
 
         }
+
     }
     private void OnTriggerStay(Collider other)
     {
-        
+
         if (playerController.canBounce && other.GetComponent<Rigidbody>() != null)
         {
             Rigidbody rb = other.GetComponent<Rigidbody>();
             rb.AddForce(launchForce, ForceMode.Impulse);
+            jack.gameObject.SetActive(true);
+            playerController.canBounce = false;
         }
 
     }
