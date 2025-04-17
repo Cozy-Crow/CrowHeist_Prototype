19d9b9313d80e993909ec7949ab8ae6f9c066fd4
horvj <horvathjack1@gmail.com>
2025-04-17 14:18:35 -0400

    moving fan now pushes outward
    moving fan now applies force outward instead of down like the ceiling fan.

--- CrowHeist_Prototype/Assets/Scripts/FanWindArea.cs
+++ CrowHeist_Prototype/Assets/Scripts/FanWindArea.cs
@@ -10,22 +10,50 @@ public class FanWindArea : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        var bladeScript = fanBlades.GetComponent<FanSpin>();
-        // Affect rigidbodies
-        if (other.attachedRigidbody != null && bladeScript.isOn)
+        if(this.gameObject.CompareTag("CeilingFan"))
         {
-            other.attachedRigidbody.AddForce(Vector3.down * windForce * 5, ForceMode.Acceleration);
-        }
-
-        // Affect CharacterController
-        var controller = other.GetComponent<CharacterController>();
-        if (controller != null && bladeScript.isOn)
-        {
-            // Assuming the controller script has a public method or flag to receive external forces
-            var player = controller.GetComponent<Controller2Point5D>(); // replace with your script
-            if (player != null)
+            var bladeScript = fanBlades.GetComponent<FanSpin>();
+            // Affect rigidbodies
+            if (other.attachedRigidbody != null && bladeScript.isOn)
             {
-                player.ApplyExternalForce(Vector3.down * windForce);
+                other.attachedRigidbody.AddForce(Vector3.down * windForce * 5, ForceMode.Acceleration);
+            }
+
+            // Affect CharacterController
+            var controller = other.GetComponent<CharacterController>();
+            if (controller != null && bladeScript.isOn)
+            {
+                // Assuming the controller script has a public method or flag to receive external forces
+                var player = controller.GetComponent<Controller2Point5D>(); // replace with your script
+                if (player != null)
+                {
+                    player.ApplyExternalForce(Vector3.down * windForce);
+                }
+            }
+        }
+        if (this.gameObject.CompareTag("MovingFan"))
+        {
+            var bladeScript = fanBlades.GetComponent<FanSpin>();
+            if (bladeScript != null && bladeScript.isOn)
+            {
+                Vector3 directionOutward = (other.transform.position - transform.position).normalized;
+
+                // Rigidbodies
+                if (other.attachedRigidbody != null)
+                {
+                    other.attachedRigidbody.AddForce(directionOutward * windForce, ForceMode.Acceleration);
+                }
+
+                // CharacterController
+                var controller = other.GetComponent<CharacterController>();
+                if (controller != null)
+                {
+                    var player = controller.GetComponent<Controller2Point5D>();
+                    if (player != null)
+                    {
+                        player.ApplyExternalForce(directionOutward * windForce);
+                    }
+                }
             }
         }
     }
