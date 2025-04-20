d24f9507b2c1381a1c42a05953338219089bd6c3
horvj <143136651+horvj@users.noreply.github.com>
2025-04-20 15:12:34 -0400

    throw distance addition
    Similar to the coffee machine, Crowley cannot chuck a quarter into the Vending Machine now and must be close to it for the Vending Machine to dispense a drink.

--- CrowHeist_Prototype/Assets/Scripts/VendingMachine.cs
+++ CrowHeist_Prototype/Assets/Scripts/VendingMachine.cs
@@ -1,24 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using KinematicCharacterController.Examples;
+
 
 public class VendingMachine : MonoBehaviour
 {
     private ItemEventManager itemEventManager;
+    private Controller2Point5D playerController;
+
+    private GameObject player;
+    private float playerDistance; 
+    [SerializeField] private float checkCooldown = 1.0f;
+    private float lastCheckTime = -Mathf.Infinity;
+
+
+
     private void Start()
     {
         itemEventManager = FindObjectOfType<ItemEventManager>();
+        player = GameObject.FindWithTag("Player");
+        if (player != null)
+        {
+            playerController = player.GetComponent<Controller2Point5D>();
+        }
         
     }
     // Start is called before the first frame update
     void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Quarter"))
+        if (Time.time - lastCheckTime >= checkCooldown)
+        {
+            DistanceCalculator();
+            lastCheckTime = Time.time;
+        }
+        if(other.CompareTag("Quarter") && playerDistance < 5f)
         {
             Destroy(other.gameObject);
             Notify();
         }
     }
+    private void DistanceCalculator()
+    {
+        if(playerController.heldObject == null)
+        {
+            playerDistance = Vector3.Distance(transform.position, player.transform.position);
+        }
+    }
+
 
     // Update is called once per frame
     public void Notify()
