9ecffa3146683d5bc0b44264de13069548368224
Hollow-Detail <43763837+Hollow-Detail@users.noreply.github.com>
2025-09-24 10:47:38 -0400

    Knife ""Bug"" Fixed
    Some walls were just missing the wall tag.

--- CrowHeist_Prototype/Assets/Scripts/Pickableup.cs
+++ CrowHeist_Prototype/Assets/Scripts/Pickableup.cs
@@ -42,17 +42,20 @@ public class Pickable : MonoBehaviour, IPickupable
         if (this.tag == "Knife")
         {
             transform.localRotation = Quaternion.Euler(90f, 90f, 0f);
+            Debug.Log("Knife Picked up");
         }
         // Adjust rotation to lay flat and face forward
         else
         {
             transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            Debug.Log("Non-Knife Picked up");
         }
+
 
         _rigidbody.isKinematic = true;
         pickedUp = true;
 
-        if(player != null)
+        if (player != null)
         {
             Controller2Point5D playerController = player.GetComponent<Controller2Point5D>();
             if (_isDirty)
@@ -62,6 +65,8 @@ public class Pickable : MonoBehaviour, IPickupable
                 Debug.Log("Player is dirty");
             }
         }
+
+
     }
     public void Drop(Vector3 position)
     {
