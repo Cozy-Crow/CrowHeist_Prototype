8184218657c0ca59c6cce4a2aa67c57834ca277b
madzwo <madzwo@iu.edu>
2025-11-03 21:02:02 -0500

    Tip + Break Roomba Vase
    Roomba vase will stay put if you run into it but when you interact it will tip over and break on the roomba dock

--- CrowHeist_Prototype/Assets/Scripts/InteractableRoombaVase.cs
+++ CrowHeist_Prototype/Assets/Scripts/InteractableRoombaVase.cs
@@ -2,14 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// Script by Mark D. - last updated 11/03/2025
+// interaction with vase above roomba dock to tip over and break roomba
+
 public class InteractableRoombaVase : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     [SerializeField] private float interactDistance;
 
-    void start()
+    public GameObject brokenPrefab;
+
+    private Rigidbody rb;
+
+    void Start()
     {
         interactDistance = 2.6f;
+        rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
@@ -29,6 +38,16 @@ public class InteractableRoombaVase : MonoBehaviour
 
     private void TipVase()
     {
-        Debug.Log("Tip Roomba Vase");
+        rb.isKinematic = false;
+        rb.AddForce(Vector3.left * 3f, ForceMode.Impulse);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("BreakVase"))
+        {
+            GameObject broken = Instantiate(brokenPrefab, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
     }
 }
