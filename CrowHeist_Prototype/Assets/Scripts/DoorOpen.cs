e836c2b26d7822d4b461e128eaad50869a26ff6f
madzwo <madzwo@iu.edu>
2025-12-06 13:36:32 -0500

    Door Open
    Door rotates around a pivot once the camera switches to the door camera.

--- CrowHeist_Prototype/Assets/Scripts/DoorOpen.cs
+++ CrowHeist_Prototype/Assets/Scripts/DoorOpen.cs
@@ -4,8 +4,35 @@ using UnityEngine;
 
 public class DoorOpen : MonoBehaviour
 {
+    // Script created by Mark D. 12/06/2025
+    // Handles the door opening part of the roomba break cutscene
+    // DoorOpen is called from the RoombaCamManager Script
+    // door rotates around pivot point at a constant speed
+
+    public Transform pivot;
+    public float openAngle = 90f;
+    public float openSpeed = 30f;
+
+    private bool isOpening = false;
+    private float currentAngle = 0f;
+
     public void OpenDoor()
     {
-        
+        if (!isOpening)
+            isOpening = true;
+    }
+
+    void Update()
+    {
+        if (!isOpening) return;
+
+        float rotate = openSpeed * Time.deltaTime;
+        float newAngle = Mathf.Min(currentAngle + rotate, openAngle);
+
+        pivot.Rotate(0f, newAngle - currentAngle, 0f);
+        currentAngle = newAngle;
+
+        if (currentAngle >= openAngle)
+            isOpening = false;
     }
 }
