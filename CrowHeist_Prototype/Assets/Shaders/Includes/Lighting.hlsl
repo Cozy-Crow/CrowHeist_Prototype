d2165c2b0841eb9e64484d5b257c4e6e37d31b8e
trevrobbbb <104037306+trevrobbbb@users.noreply.github.com>
2026-01-29 06:12:20 -0500

    Cel Shader Ramp Texture

--- CrowHeist_Prototype/Assets/Shaders/Includes/Lighting.hlsl
+++ CrowHeist_Prototype/Assets/Shaders/Includes/Lighting.hlsl
@@ -1,6 +1,7 @@
 #ifndef CUSTOM_LIGHTING_INCLUDED
 #define CUSTOM_LIGHTING_INCLUDED
 
+
 void CalculateMainLight_float(float3 WorldPos, out float3 Direction, out float3 Color){
 #if SHADERGRAPH_PREVIEW
     Direction = float3(0.5, 0.5, 0);
