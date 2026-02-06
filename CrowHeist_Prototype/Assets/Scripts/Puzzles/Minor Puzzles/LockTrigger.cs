5e9a7e19cf482996dae9ee24b149331f3810321a
lopruitt7903 <147568471+lopruitt7903@users.noreply.github.com>
2026-02-06 17:19:33 -0500

    Traversal Checkpoint Polish
    - Key is now required to be thrown at the lock to open
    - starting implementation for unlock motion

--- CrowHeist_Prototype/Assets/Scripts/Puzzles/Minor Puzzles/LockTrigger.cs
+++ CrowHeist_Prototype/Assets/Scripts/Puzzles/Minor Puzzles/LockTrigger.cs
@@ -4,14 +4,32 @@ public class LockTrigger : MonoBehaviour
 {
     [SerializeField] private string keyTag = "";
     [SerializeField] private TraversalCheckpoint checkpoint;
+    [SerializeField] private float minVelocity = 3f;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(keyTag) && checkpoint != null)
         {
-            gameObject.AddComponent<Rigidbody>();
-            gameObject.GetComponent<BoxCollider>().isTrigger = false;
-            checkpoint.MoveObjectsToEndPositions();
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            if (rb != null && rb.velocity.magnitude >= minVelocity)
+            {
+                UnlockMotion(other.transform);
+                checkpoint.MoveObjectsToEndPositions();
+            }
         }
+    }
+
+    private void UnlockMotion(Transform key)
+    {
+        key.position = transform.position;
+        key.Rotate(0, 90, 0);
+        key.SetParent(transform);
+        
+        Rigidbody keyRb = key.GetComponent<Rigidbody>();
+        if (keyRb) Destroy(keyRb);
+        
+        Rigidbody lockRb = GetComponent<Rigidbody>();
+        if (!lockRb) lockRb = gameObject.AddComponent<Rigidbody>();
+        lockRb.useGravity = true;
     }
 }
