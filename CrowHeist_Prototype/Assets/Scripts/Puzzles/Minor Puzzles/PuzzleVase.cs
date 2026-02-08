4b06a9edbc74141ce8633956a880b48f7ba68066
madzwo <madzwo@iu.edu>
2026-02-07 20:47:37 -0500

    Break The Vases Puzzle

---
+++ CrowHeist_Prototype/Assets/Scripts/Puzzles/Minor Puzzles/PuzzleVase.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleVase : MonoBehaviour
+{
+    // Script by Mark D. 2/7/26
+    // break the vases puzzle: vase will break with a certain force and spawn broken prefab
+    // the broken peices will explode and will not effect player movement
+
+    public float breakSpeed = 10f;
+    public GameObject brokenVasePrefab;
+    public GameObject coinPrefab;
+
+    public float explodeForce = 2f;
+
+    private bool broken;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (broken) return;
+
+        if (collision.relativeVelocity.magnitude >= breakSpeed)
+        {
+            Break();
+        }
+    }
+
+    void Break()
+    {
+        broken = true;
+
+        GameObject brokenInstance = Instantiate(brokenVasePrefab, transform.position, transform.rotation);
+
+        // gets the broken peices from the broken vase prefab right after instantiated
+        foreach (Rigidbody rb in brokenInstance.GetComponentsInChildren<Rigidbody>())
+        {
+            rb.AddForce(Random.onUnitSphere * explodeForce, ForceMode.Impulse);
+        }
+
+        // If this is the last PuzzleVase in the scene
+        if (FindObjectsOfType<PuzzleVase>().Length == 1 && coinPrefab)
+        {
+            Instantiate(coinPrefab, transform.position, Quaternion.identity);
+        }
+
+        Destroy(gameObject);
+    }
+}
