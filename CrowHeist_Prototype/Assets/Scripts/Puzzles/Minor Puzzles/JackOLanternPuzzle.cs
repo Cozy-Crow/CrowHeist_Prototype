876ebef2af6561734ada5c2a860789fdafa8752a
CJSimmons08 <125491709+CJSimmons08@users.noreply.github.com>
2026-02-11 16:10:25 -0500

    Jack-o-Lantern Prototype functional
    Prototype version of the jack-o-lantern puzzle working. Need to add the visual effects

--- CrowHeist_Prototype/Assets/Scripts/Puzzles/Minor Puzzles/JackOLanternPuzzle.cs
+++ CrowHeist_Prototype/Assets/Scripts/Puzzles/Minor Puzzles/JackOLanternPuzzle.cs
@@ -1,12 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using FMOD;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class JackOLanternPuzzle : MonoBehaviour
 {
     
     [SerializeField] private Mesh jackOLanternPuzzleMesh;
     [SerializeField] private Material jackOLanternPuzzleMaterial;
+    [SerializeField] private Vector3 jackOLanternScale;
     
     private MeshRenderer pumpkinMeshRenderer;
     private MeshFilter pumpkinMeshFilter;
@@ -27,9 +31,36 @@ public class JackOLanternPuzzle : MonoBehaviour
         }
     }
 
+    // public void OnTriggerEnter(Collider other)
+    // {
+    //     Debug.Log(other.gameObject.name);
+    //     if (other.CompareTag("Dart"))
+    //     {
+    //         if (other.gameObject.CompareTag("Dart"))
+    //         {
+    //             Debug.Log("dart detected");
+    //             ChangeToJackOLantern();
+    //         }
+    //         
+    //     }
+    // }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.CompareTag("Dart"))
+        {
+            Debug.Log("dart detected");
+            ChangeToJackOLantern();
+        }
+    }
+
+
+
     private void ChangeToJackOLantern()
     {
         pumpkinMeshRenderer.material = jackOLanternPuzzleMaterial;
         pumpkinMeshFilter.mesh = jackOLanternPuzzleMesh;
+        gameObject.transform.localScale = jackOLanternScale;
     }
 }
