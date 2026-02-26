6e7adfe813f8b3195effc5df11e89d626d724c27
horvj <143136651+horvj@users.noreply.github.com>
2026-02-26 17:28:50 -0500

    fix issues with collisions

--- CrowHeist_Prototype/Assets/Scripts/Trinkets/HeistZonePlayerBlock.cs
+++ CrowHeist_Prototype/Assets/Scripts/Trinkets/HeistZonePlayerBlock.cs
@@ -1,14 +1,28 @@
 using UnityEngine;
+using KinematicCharacterController.Examples;
 
 public class HeistZoneBlocker : MonoBehaviour
 {
+    private Controller2Point5D playerController;
+
+    void Start()
+    {
+        playerController = FindObjectOfType<Controller2Point5D>();
+    }
     void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("Player")) return;
+
         Debug.Log("Trigger fired by: " + other.gameObject.name + " on layer: " + other.gameObject.layer);
         
         RespawnObject respawnObject = other.GetComponentInParent<RespawnObject>();
         if (respawnObject != null)
         {
+            Pickable pickup = respawnObject.gameObject.GetComponent<Pickable>();
+            if(pickup != null && pickup.pickedUp)
+            {
+                playerController.Drop();
+            }
             respawnObject.Respawn();
         }
     }
