8ce6b5aab7ca178193199d8f222e5b79cb2fbeb8
madzwo <madzwo@iu.edu>
2026-04-06 19:05:08 -0400

    debugging door collision

--- CrowHeist_Prototype/Assets/Scripts/BreakDoor.cs
+++ CrowHeist_Prototype/Assets/Scripts/BreakDoor.cs
@@ -7,6 +7,9 @@ public class BreakDoor : MonoBehaviour
     [SerializeField] private GameObject door;
     [SerializeField] private GameObject brokenDoor;
 
+    [SerializeField] private Transform roomba;
+    [SerializeField] private float breakDistance = 9f;
+
     private bool isAttacking = false;
 
     public void SetAttacking()
@@ -14,16 +17,33 @@ public class BreakDoor : MonoBehaviour
         isAttacking = true;
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void Update()
     {
         if (!isAttacking) return;
 
-        if (other.CompareTag("BreakDoor"))
+        float distance = Vector3.Distance(roomba.position, door.transform.position);
+        UnityEngine.Debug.Log("Roomba distance to door: " + distance);
+
+        if (distance <= breakDistance)
         {
+            UnityEngine.Debug.Log("Roomba broke the door!");
             brokenDoor.SetActive(true);
-            Destroy(door);
+            door.SetActive(false);
             isAttacking = false;
         }
     }
+
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     if (!isAttacking) return;
+
+    //     if (other.CompareTag("RoombaTrigger"))
+    //     {
+    //         UnityEngine.Debug.Log("roomba hit door");
+    //         brokenDoor.SetActive(true);
+    //         Destroy(door);
+    //         isAttacking = false;
+    //     }
+    // }
 }
 
