6f38414183858772b87a10dfc2163e655806e046
Willrocker11 <Wheilbraun@gmail.com>
2026-04-15 10:47:45 -0400

    4/15
    attempt to get button SFX working

---
+++ CrowHeist_Prototype/Assets/ButtonSFX.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using UnityEngine.UI;
+
+public class ButtonSFX : MonoBehaviour
+{
+    [SerializeField] private EventReference buttonClick;
+
+    private int buttonSFXClick = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  if ( == 1)
+        //  {
+            
+        //  }
+    }
+}
