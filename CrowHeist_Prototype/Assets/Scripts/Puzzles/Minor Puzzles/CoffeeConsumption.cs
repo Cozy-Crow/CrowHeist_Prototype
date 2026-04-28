85facc142e51c0143dc8547fb3121e96b44a349e
horvj <143136651+horvj@users.noreply.github.com>
2026-04-28 13:27:27 -0400

    remove weird speed boost bug

--- CrowHeist_Prototype/Assets/Scripts/Puzzles/Minor Puzzles/CoffeeConsumption.cs
+++ CrowHeist_Prototype/Assets/Scripts/Puzzles/Minor Puzzles/CoffeeConsumption.cs
@@ -1,68 +1,68 @@
-using System.Collections;
-using UnityEngine;
+// using System.Collections;
+// using UnityEngine;
 
-namespace KinematicCharacterController.Examples
-{
-    public class CoffeeConsumption : MonoBehaviour
-    {
-        private Controller2Point5D controller;
+// namespace KinematicCharacterController.Examples
+// {
+//     public class CoffeeConsumption : MonoBehaviour
+//     {
+//         private Controller2Point5D controller;
 
-        private void Start()
-        {
-            controller = FindObjectOfType<Controller2Point5D>();
-        }
+//         private void Start()
+//         {
+//             controller = FindObjectOfType<Controller2Point5D>();
+//         }
 
-        public void TryConsumeCoffee()
-        {
-            if (controller != null && controller.heldObject == this.GetComponent<Rigidbody>() && Input.GetKeyDown(KeyCode.E) && !controller.isSpeedBoosted)
-        {
-            StartCoroutine(SpeedBoost());
-        }
-        }
+//         public void TryConsumeCoffee()
+//         {
+//             if (controller != null && controller.heldObject == this.GetComponent<Rigidbody>() && Input.GetKeyDown(KeyCode.E) && !controller.isSpeedBoosted)
+//         {
+//             StartCoroutine(SpeedBoost());
+//         }
+//         }
 
-        private IEnumerator SpeedBoost()
-        {
-            GameObject coffeeDrink = null;
-            GameObject coffee = null;
+//         private IEnumerator SpeedBoost()
+//         {
+//             GameObject coffeeDrink = null;
+//             GameObject coffee = null;
 
-            if (controller.heldObject == null || !controller.heldObject.CompareTag("Dashable"))
-            {
-                yield break;
-            }
+//             if (controller.heldObject == null || !controller.heldObject.CompareTag("Dashable"))
+//             {
+//                 yield break;
+//             }
 
-            if (controller.heldObject.CompareTag("Dashable"))
-            {
-                coffeeDrink = controller.heldObject.gameObject;
-            }
+//             if (controller.heldObject.CompareTag("Dashable"))
+//             {
+//                 coffeeDrink = controller.heldObject.gameObject;
+//             }
 
-            foreach (Transform child in controller.GetComponentsInChildren<Transform>(true))
-            {
-                if (child.name == "CoffeeLiquid")
-                {
-                    coffee = child.gameObject;
-                    break;
-                }
-            }
+//             foreach (Transform child in controller.GetComponentsInChildren<Transform>(true))
+//             {
+//                 if (child.name == "CoffeeLiquid")
+//                 {
+//                     coffee = child.gameObject;
+//                     break;
+//                 }
+//             }
 
-            controller.isSpeedBoosted = true;
-            controller.moveSpeed *= controller.speedBoostMultiplier;
+//             controller.isSpeedBoosted = true;
+//             controller.moveSpeed *= controller.speedBoostMultiplier;
 
-            yield return new WaitForSeconds(controller.speedBoostDuration);
+//             yield return new WaitForSeconds(controller.speedBoostDuration);
 
-            controller.moveSpeed = controller.normalMoveSpeed;
-            controller.isSpeedBoosted = false;
+//             controller.moveSpeed = controller.normalMoveSpeed;
+//             controller.isSpeedBoosted = false;
             
-            if (coffee != null)
-            {
-                coffee.transform.localPosition = new Vector3(0, 0.0076f, 0);
-            }
+//             if (coffee != null)
+//             {
+//                 coffee.transform.localPosition = new Vector3(0, 0.0076f, 0);
+//             }
             
-            if (coffeeDrink != null)
-            {
-                coffeeDrink.tag = "Mug";
-            }
+//             if (coffeeDrink != null)
+//             {
+//                 coffeeDrink.tag = "Mug";
+//             }
             
-            controller.Drop();
-        }
-    }
-}
+//             controller.Drop();
+//         }
+//     }
+// }
