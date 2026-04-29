bdaffaa886ba004b757855c5a320d66879a3dcb3
Aaron <Mcread1019@gmail.com>
2026-04-28 20:16:08 -0400

    fixed controls menu

--- CrowHeist_Prototype/Assets/ControlsMenu.cs
+++ CrowHeist_Prototype/Assets/ControlsMenu.cs
@@ -1,21 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControlsMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject controlsPanel;
+    [SerializeField] private Button backButton;
+
+    private bool _openedBeforeStart = false;
+
     public bool IsOpen { get; private set; }
+
+    private void Start()
+    {
+        if (backButton != null)
+            backButton.onClick.AddListener(CloseControls);
+
+        if (controlsPanel != null && !_openedBeforeStart)
+            controlsPanel.SetActive(false);
+    }
 
     public void OpenControls()
     {
-        gameObject.SetActive(true);
+        _openedBeforeStart = true;
+        if (controlsPanel != null)
+            controlsPanel.SetActive(true);
         IsOpen = true;
     }
 
     public void CloseControls()
     {
-        gameObject.SetActive(false);
+        if (controlsPanel != null)
+            controlsPanel.SetActive(false);
         IsOpen = false;
     }
-
 }
